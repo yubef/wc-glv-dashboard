bde69955c70a28d4bcc8f6249fd78ab6a4152d2d
yubef <117343714+yubef@users.noreply.github.com>
2026-06-12 09:52:41 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Dashboard Arisan Piala Dunia 2026",
-  description: "Live dashboard poin arisan Piala Dunia 2026"
+  title: "Dashboard Piala Dunia 2026",
+  description: "Dashboard poin Piala Dunia 2026"
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
